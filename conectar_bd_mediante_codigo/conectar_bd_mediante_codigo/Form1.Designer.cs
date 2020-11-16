7ff1c4989b06635b51391dd737b8b5431b0ddc04
davidantoniol <57524676+davidantoniol@users.noreply.github.com>
2020-11-16 09:25:02 +0100

    Error solucionado en el fichero Program.cs

---
+++ conectar_bd_mediante_codigo/conectar_bd_mediante_codigo/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace conectar_bd_mediante_codigo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(607, 160);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 278);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 18;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(166, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 36);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Consultar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(795, 308);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 38);
+            this.button2.TabIndex = 34;
+            this.button2.Text = "Insertar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(795, 245);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(145, 26);
+            this.textBox7.TabIndex = 33;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(670, 251);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 20);
+            this.label7.TabIndex = 32;
+            this.label7.Text = "Población";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(670, 211);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 20);
+            this.label6.TabIndex = 31;
+            this.label6.Text = "Código Postal";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(670, 171);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 20);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "Dirección";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(670, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 20);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "Teléfono";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(670, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 20);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "Apellidos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(670, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Nombre";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(670, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 20);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Id_contacto";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(795, 205);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(145, 26);
+            this.textBox6.TabIndex = 25;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(795, 165);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(145, 26);
+            this.textBox5.TabIndex = 24;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(795, 127);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(145, 26);
+            this.textBox4.TabIndex = 23;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(795, 89);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(145, 26);
+            this.textBox3.TabIndex = 22;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(795, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(145, 26);
+            this.textBox2.TabIndex = 21;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(795, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 26);
+            this.textBox1.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(997, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ conectar_bd_mediante_codigo/conectar_bd_mediante_codigo/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace conectar_bd_mediante_codigo
+{
+    public partial class Form1 : Form
+    {
+        static MySqlConnection Conex = new MySqlConnection();
+        static string serv = "Server=localhost;";
+        static string db = "Database=agenda;";
+        static string usuario = "UID=root;";
+        static string pwd = "Password=Davidlr08 ;";
+        string CadenaDeConexion = serv + db + usuario + pwd;
+        string id_contac = null;
+        string nom = null;
+        string apellido = null;
+        string telefo = null;
+        string direccion = null;
+        string cp = null;
+        string poblacion = null;
+        string query = null;
+        public Form1()
+        {
+            InitializeComponent();
+            actualizar_tabla();
+            guardar_en_combobox();
+        }
+
+        public void Conectar()
+        {
+            try
+            {
+                Conex.ConnectionString = CadenaDeConexion;
+                Conex.Open();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ocurrio un error al conectar a la BD");
+                throw;
+            }
+        }
+        public static void Desconectar()
+        {
+            Conex.Close();
+        }
+
+        public void insertar_contactos(String query)
+        {
+            Conectar();
+            MySqlCommand insertar = new MySqlCommand(query, Conex);
+            insertar.ExecuteNonQuery();
+            Desconectar();
+            actualizar_tabla();
+            guardar_en_combobox();
+        }
+
+        public void actualizar_tabla()
+        {
+            MySqlDataAdapter adaptador;
+            DataTable table = new DataTable();
+            Conectar();
+            try
+            {
+                adaptador = new MySqlDataAdapter("Select id_contacto,nombre,apellidos from contactos", Conex);
+                adaptador.Fill(table);
+                dataGridView1.DataSource = table;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se ha actualizado la tabla.");
+            }
+            Desconectar();
+        }
+
+        public void guardar_en_combobox()
+        {
+            MySqlCommand adaptador;
+            Conectar();
+            try
+            {
+                adaptador = new MySqlCommand("Select cp from contactos", Conex);
+                MySqlDataReader reader = adaptador.ExecuteReader();
+                comboBox1.Items.Clear();
+                while (reader.Read())
+                {
+
+                    comboBox1.Items.Add(Convert.ToString(reader["cp"]));
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se ha a guardado en el combobox.");
+            }
+            Desconectar();
+        }
+
+        public void consulta(string consulta)
+        {
+            MySqlDataAdapter adaptador;
+            DataTable table = new DataTable();
+            Conectar();
+            try
+            {
+                adaptador = new MySqlDataAdapter("Select id_contacto,nombre,apellidos from contactos where(cp='" + consulta + "')", Conex);
+                adaptador.Fill(table);
+                dataGridView1.DataSource = table;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se ha actualizado la tabla.");
+            }
+            Desconectar();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            id_contac = textBox1.Text;
+            nom = textBox2.Text;
+            apellido = textBox3.Text;
+            telefo = textBox4.Text;
+            direccion = textBox5.Text;
+            cp = textBox6.Text;
+            poblacion = textBox7.Text;
+
+            query = "INSERT INTO contactos(id_contacto,nombre,apellidos,telefono,direccion,cp,poblacion) VALUES (" + id_contac + " ,'" + nom + "','" + apellido + "','" + telefo + "','" + direccion + "','" + cp + "','" + poblacion + "')";
+            insertar_contactos(query);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string consulta_cp = Convert.ToString(comboBox1.SelectedItem);
+            consulta(consulta_cp);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
