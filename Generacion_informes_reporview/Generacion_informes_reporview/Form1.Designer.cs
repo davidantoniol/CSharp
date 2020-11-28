bfd49690889b61d77fe1278e3a2e8aedd1fc00f9
davidantonil <lopezreinosaantoniodavid@gmail.com>
2020-11-28 20:39:17 +0100

    Ejercicio generar informe con reportview

---
+++ Generacion_informes_reporview/Generacion_informes_reporview/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Generacion_informes_reporview
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.ARTICULOSBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.pedidosDataSet = new Generacion_informes_reporview.pedidosDataSet();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.ARTICULOSTableAdapter = new Generacion_informes_reporview.pedidosDataSetTableAdapters.ARTICULOSTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.ARTICULOSBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pedidosDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ARTICULOSBindingSource
+            // 
+            this.ARTICULOSBindingSource.DataMember = "ARTICULOS";
+            this.ARTICULOSBindingSource.DataSource = this.pedidosDataSet;
+            // 
+            // pedidosDataSet
+            // 
+            this.pedidosDataSet.DataSetName = "pedidosDataSet";
+            this.pedidosDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource2.Name = "DataSet1";
+            reportDataSource2.Value = this.ARTICULOSBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource2);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "Generacion_informes_reporview.Inventario.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(800, 450);
+            this.reportViewer1.TabIndex = 1;
+            // 
+            // ARTICULOSTableAdapter
+            // 
+            this.ARTICULOSTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ARTICULOSBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pedidosDataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource ARTICULOSBindingSource;
+        private pedidosDataSet pedidosDataSet;
+        private pedidosDataSetTableAdapters.ARTICULOSTableAdapter ARTICULOSTableAdapter;
+    }
+}
+
---
+++ Generacion_informes_reporview/Generacion_informes_reporview/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Generacion_informes_reporview
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'pedidosDataSet.ARTICULOS' Puede moverla o quitarla según sea necesario.
+            this.ARTICULOSTableAdapter.Fill(this.pedidosDataSet.ARTICULOS);
+
+            this.reportViewer1.RefreshReport();
+
+            this.reportViewer2.RefreshReport();
+        }
+    }
+}
