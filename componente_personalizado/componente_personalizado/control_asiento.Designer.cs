440b8e9264bc0da32f69bb9fa35a3f691b1e01ab
davidantonil <lopezreinosaantoniodavid@gmail.com>
2020-11-28 20:41:21 +0100

    Ejercicio componente personalizado

---
+++ componente_personalizado/componente_personalizado/control_asiento.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace componente_personalizado
+{
+    partial class control_asiento
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::componente_personalizado.Properties.Resources.asiento1;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(229, 231);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 252);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "D";
+            // 
+            // control_asiento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "control_asiento";
+            this.Size = new System.Drawing.Size(380, 314);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ componente_personalizado/componente_personalizado/control_asiento.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace componente_personalizado
+{
+    public partial class control_asiento : UserControl
+    {
+        int contador = 0;
+        public control_asiento()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (contador == 0)
+            {
+                contador = 1;
+            }
+            else if (contador == 2)
+            {
+                contador = 1;
+            }
+            else if (contador == 1)
+            {
+                contador = 2;
+            }
+            if (contador == 1)
+            {
+                pictureBox1.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("asientos");
+                label1.Text = "O";
+            }
+            else if (contador == 2)
+            {
+                pictureBox1.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("asiento1");
+                label1.Text = "D";
+            }
+        }
+    }
+}
