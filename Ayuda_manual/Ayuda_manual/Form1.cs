4b21aa3a7da026b3fc8fa147a95b88c46c0fb1db
davidantoniol <57524676+davidantoniol@users.noreply.github.com>
2021-01-13 10:42:36 +0100

    Creación de un menú en el cuál lanzamos un documento de ayuda.

---
+++ Ayuda_manual/Ayuda_manual/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ayuda_manual
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Help.ShowHelp(this, "C:/Users/adavi/Desktop/Ayuda_manual/DocumentoAyuda/Antonio David Lopez Reinosa.chm");
+        }
+    }
+}
