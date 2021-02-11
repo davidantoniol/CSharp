8abe4d5c36e45ee42bb65dbcde52992c914caec8
davidantoniol <57524676+davidantoniol@users.noreply.github.com>
2021-02-11 13:25:10 +0100

    Botón para salir personalizado

---
+++ BotonSakir/ButtonSalir.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace BotonSakir
+{
+    partial class ButtonSalir
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BotonSakir.Properties.Resources.icons8_shutdown_40px;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(98, 86);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+            // 
+            // ButtonSalir
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "ButtonSalir";
+            this.Size = new System.Drawing.Size(98, 86);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ BotonSakir/ButtonSalir.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BotonSakir
+{
+    public partial class ButtonSalir: UserControl
+    {
+        ToolTip toolTip1 = new ToolTip();
+        public ButtonSalir()
+        {
+            InitializeComponent();
+            this.toolTip1.InitialDelay = 500;
+            this.toolTip1.ReshowDelay = 100;
+            this.toolTip1.AutoPopDelay = 5000;
+
+            this.toolTip1.ShowAlways = true;
+            this.toolTip1.IsBalloon = true;
+
+            this.toolTip1.AutomaticDelay = 800;
+            this.toolTip1.AutoPopDelay = 800;
+            this.toolTip1.InitialDelay = 122;
+
+            this.toolTip1.SetToolTip(this.pictureBox1, "Salir.");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox1_MouseHover(object sender, EventArgs e)
+        {
+            String openPDFFile = @"C:\Users\adavi\Desktop\BotonSakir\Resources\icons8_shutdown_40px_1.png";
+            
+            pictureBox1.Image = Image.FromFile(openPDFFile);
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            String openPDFFile = @"C:\Users\adavi\Desktop\BotonSakir\Resources\icons8_shutdown_40px.png";
+            
+            pictureBox1.Image = Image.FromFile(openPDFFile);
+        }
+    }
+}
